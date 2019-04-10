8216513c7b696b380554ecfa1500e4e4dbb8e340
Abhilash kollipara <sahithyabhilash@gmail.com>
2019-04-10 18:19:36 +0530

    Add files via upload

---
+++ isomorphic.py
@@ -0,0 +1,9 @@
+def isIso(a,b):
+        if(len(a) != len(b)):
+                return false
+        x=[a.count(char1) for char1 in a]
+        y=[b.count(char1) for char1 in b]
+        return x==y                   
+string1 = input()
+string2 = input()
+print(isIso(string1,string2))
