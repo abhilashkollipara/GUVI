cae11d4f4fe4635e806bd4a501a314d4f1a07915
Abhilash kollipara <sahithyabhilash@gmail.com>
2019-05-16 12:39:04 +0530

    Add files via upload

---
+++ week.py
@@ -0,0 +1,12 @@
+day= input().split()
+lis1={"Monday","Tuesday","Wednesday","Thursday","Friday"}
+lis2={"Saturday","Sunday"}
+i=0
+n=len(day)
+for i in range(n):
+
+    if day[i] in lis1:
+        print("No")
+    
+    elif day[i] in lis2:
+         print("Yes")
