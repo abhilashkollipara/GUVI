902ce2f9bfedfe5c2aea4ad9b4d5f3baebd37123
Abhilash kollipara <sahithyabhilash@gmail.com>
2019-08-02 04:19:21 +0530

    Add files via upload

--- pro_1.py
+++ pro_1.py
@@ -1,14 +1,19 @@
-str1=input()
-str2=input()
-n1 = len(str1) 
-n2 = len(str2) 
-result = ""
-i=0
-j=0
-while(i <= n1 - 1 and j <= n2 - 1):
-    if (str1[i] != str2[j]): 
-        break
-    result += (str1[i]) 
-    i += 1
-    j += 1
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(input())
+arr.sort(reverse = False)     
+n1 = len(arr[0])
+t1=arr[0]
+n2 = len(arr[n-1])
+t2=arr[n-1]
+result = "" 
+j = 0
+i = 0
+while(i <= n1 - 1 and j <= n2 - 1): 
+        if (t1[i]!=t2[j]): 
+            break
+        result += (t1[i]) 
+        i += 1
+        j += 1
 print(result)
